645cda655c3e2ce09a958263328109e371dc2565
cyrilico <jpdamas15@gmail.com>
2019-10-02 13:58:17 +0100

    Remove previous code from main

--- main.py
+++ main.py
@@ -18,28 +18,6 @@ def parse_args():
 
     return vars(parser.parse_args())
 
-# def build_test_call_argument(argument):
-#     """Creates a valid function call argument (adapts to argument type)
-#     """
-#     if type(argument) is list:
-#         return '({}[]){{ {} }}'.format(type(argument[0]).__name__, ','.join([str(e) for e in argument]))
-#     else:
-#         return str(argument)
-
-# def build_test_call(func_name, inputs):
-#     """Creates a function call given a specific test case and prints result to stdout
-#     """
-#     return 'printf("%d\\n", {}({}));'.format(func_name, ','.join([build_test_call_argument(i) for i in inputs]))
-
-# def build_arg_list(params):
-#     """Creates function argument list given its parameter types
-#     """
-#     return ','.join(['{} {}'.format(
-#                             '{}*'.format(arg.replace('array ', '')) if 'array' in arg 
-#                                 else 'char*' if arg == 'string' else arg,
-#                             'arg{}'.format(arg_idx)
-#                             ) for arg_idx, arg in enumerate(params)])
-
 def build_subroutine_c_file(name, definition, test_cases):
     """Creates a C file that will run all the test inputs for a given subroutine
     """
